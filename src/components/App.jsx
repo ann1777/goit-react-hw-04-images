968713b108f25b664b7164e08a67025fcb2d6b3f
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-05-31 22:04:06 +0300

    Added loader spinner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,120 +3,144 @@ import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import { LoadButton } from './LoadButton/LoadButton';
 import Modal from './Modal/Modal';
-// import Loader from './Loader/Loader';
-import  { ToastContainer, toast } from 'react-toastify';
+import Loader from './Loader/Loader';
+import { ToastContainer, toast } from 'react-toastify';
 import handleFetch from '../services/pixabayapi';
 
-  export const App = () => {
-    const [images, setImages] = useState([]);
-    const [inputValue, setInputValue] = useState('');
-    const [status, setStatus] = useState('idle');
-    const [showModal, setShowModal] = useState(false);
-    const [page, setPage] = useState(1);
-    const [url, setUrl] = useState('');
-    const [alt, setAlt] = useState('');
-    const [totalPage] = useState(1);
-    const resetPage = () => {
-      setPage(1);
-    };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [status, setStatus] = useState('idle');
+  const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPage] = useState(1);
+  const [url, setUrl] = useState('');
+  const [alt, setAlt] = useState('');
+  const resetPage = () => {
+    setPage(1);
+  };
 
   useEffect(() => {
     if (status === 'pending') {
-      setStatus('loading');
+      setIsLoading();
       handleFetch(inputValue)
-
       .then(data => onHandleData(data.hits))
-      .catch(error => console.log(error));
-  }
-}, [inputValue, status]);
+      .catch(error => console.log(error))
+      .finally(toggleLoading());
+    }
+  }, [inputValue, status]);
 
-function onHandleData(data) {
-  if (data.length === 12) {
-    setStatus('loaded');
-    setImages(prevData => [...prevData, ...data]);
-    return;
-  }
-  if (data.length === 0) {
-    setStatus('rejected');
-    setImages([]);
-    return;
-  }
-  setStatus('idle');
-  setImages(prevData => [...prevData, ...data]);
-  return;
-}
-        // .then(({hits}) => {
-        //   if (hits.length === 12) {
-        //     setStatus('loaded');
-        //     setImages(prevData => [...prevData, ...hits]);
-        //     return;
+        // .then(response => {
+        //   if (page > 1) {
+        //     setImages(prevState => [...prevState, ...response.hits]);
+        //   } else {
+        //     setImages([...response.hits]);
+        //     setTotalPage(Math.ceil(response.total - 12));
         //   }
-        //   else if (hits.length === 0) {
-        //       toast.info('No images found.', {
-        //         position: toast.POSITION.BOTTOM_CENTER,
-        //       });
-        //       setStatus('rejected');
-        //       setImages([]);
-        //       return;
-        //     }
-        //     setStatus('idle');
-        //     setImages(prevData => [...prevData, ...hits]);
-        //     return;
-        //   });
-        // }}, [inputValue, status]);
+        // })
+        // .catch(
+        //   error => {
+        //     setErr(error);
+        //     console.log(err);
+        //   },
+        //   [inputValue, page, err]
+        // );
+    // }
 
-  const onLoadMore = () => {
-    setStatus({ status: 'pending' });
-    setPage(page + 1);
-  };
+    //       } onHandleData(data.hits))
+    //       .catch(error => console.log(error));
+    //   }
+    // }, [inputValue, status]);
 
-  const onSubmit = inputValue => {
-    resetPage();
-    setStatus('pending');
-    setImages([]);
-    setInputValue(inputValue);
-    if (!inputValue) {
-      toast.error('Please enter a search query');
+    function onHandleData(data) {
+      if (data.length === 12) {
+        setStatus('loaded');
+        setImages(prevData => [...prevData, ...data]);
+        return;
+      }
+      if (data.length === 0) {
+        setStatus('rejected');
+        setImages([]);
+        return;
+      }
+      setStatus('idle');
+      setImages(prevData => [...prevData, ...data]);
       return;
     }
+    // .then(({hits}) => {
+    //   if (hits.length === 12) {
+    //     setStatus('loaded');
+    //     setImages(prevData => [...prevData, ...hits]);
+    //     return;
+    //   }
+    //   else if (hits.length === 0) {
+    //       toast.info('No images found.', {
+    //         position: toast.POSITION.BOTTOM_CENTER,
+    //       });
+    //       setStatus('rejected');
+    //       setImages([]);
+    //       return;
+    //     }
+    //     setStatus('idle');
+    //     setImages(prevData => [...prevData, ...hits]);
+    //     return;
+    //   });
+    // }}, [inputValue, status]);
+
+    const onLoadMore = () => {
+      setStatus({ status: 'pending' });
+      setPage(page + 1);
+    };
+
+    const onSubmit = inputValue => {
+      resetPage();
+      setStatus('pending');
+      setImages([]);
+      setInputValue(inputValue);
+      if (!inputValue) {
+        toast.error('Please enter a search query');
+        return;
+      }
+    };
+
+    const handleClick = (url, alt) => {
+      setShowModal(prevState => !prevState);
+      setUrl(url);
+      setAlt(alt);
+    };
+
+    const onCloseModal = () => {
+      setShowModal(false);
+    };
+
+    const toggleLoading = () => {
+      setIsLoading(prevState => !prevState);
+    };
+
+    return (
+      <>
+        <Searchbar
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={inputValue}
+          onSubmit={onSubmit}
+        />
+        {isLoading && <Loader/>}
+        <ImageGallery images={images} handleClick={handleClick} />
+        {showModal && <Modal img={url} alt={alt} onClose={onCloseModal} />}
+        {status === 'loaded' && <LoadButton onLoadMore={onLoadMore} />}
+        {status === 'rejected' && (
+          <div>
+            Your generic alert to promt you that there are no images found, but
+            I was too lazy to style it. Hell, at least it removed that "Load
+            More" button from showing
+          </div>
+        )}
+        {page < totalPage && <LoadButton onLoadMore={onLoadMore} />}
+        <ToastContainer autoClose={1000} />
+      </>
+    );
   };
-
-  const handleClick = (url, alt) => {
-    setShowModal(prevState => !prevState);
-    setUrl(url);
-    setAlt(alt);
-  };
-
-  const onCloseModal = () => {
-    setShowModal(false);
-  };
-
-
-  return (
-    <>
-      <Searchbar
-        type="text"
-        autoComplete="off"
-        autoFocus
-        placeholder="Search images and photos"
-        value={inputValue}
-        onSubmit={onSubmit}
-      />
-      <ImageGallery images={images} handleClick={handleClick}/>
-      {showModal &&
-          <Modal img={url} alt={alt} onClose={onCloseModal} />}
-      {status === 'loaded' && (
-        <LoadButton onLoadMore={onLoadMore} />
-      )}
-      {status === 'rejected' && (
-        <div>
-          Your generic alert to promt you that there are no images found, but
-          I was too lazy to style it. Hell, at least it removed that "Load
-          More" button from showing
-        </div>
-      )}
-      {(page < totalPage) && <LoadButton onLoadMore={onLoadMore} />}
-      <ToastContainer autoClose={1000} />
-    </>
-  );
-}
