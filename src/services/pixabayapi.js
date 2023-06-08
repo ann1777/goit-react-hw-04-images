fab0b90e0e9873d09d9502ed613b73d45ad22651
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-06-08 16:47:18 +0300

    Added page to handleFetch method params

--- src/services/pixabayapi.js
+++ src/services/pixabayapi.js
@@ -1,19 +1,17 @@
-let PAGE_COUNTER = 1;
-export default async function handleFetch (inputValue) {
+export default async function handleFetch (inputValue, page) {
   const BASE_URL = 'https://pixabay.com/api/';
   const searchParam = new URLSearchParams({
     key: '35889696-234245940e7cec3ffc17b1751',
     q: `${inputValue}`,
     image_type: 'photo',
     orientation: 'horizontal',
-    page: `${PAGE_COUNTER}`,
+    page: `${page}`,
     per_page: 12,
   });
 
   const response = await fetch(`${BASE_URL}?${searchParam}`);
   if (response.ok) {
-    PAGE_COUNTER += 1;
     return response.json();
   }
-  throw new Error('ERROR');
+  throw new Error('Sorry, we couldn’t find any image for your search ', `${inputValue}`);
 }
